c0a8fd10c37c5094f43eaee173bb0989895d2862
cpcwood <cpcwood@outlook.com>
2019-12-05 17:41:05 +0000

    Fizzbuzz pairing challenge completed

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,12 @@
 def fizzbuzz(number)
   # FizzBuzz code
+  if number % 15 == 0
+    return 'fizzbuzz'
+  elsif number % 3 == 0
+    return 'fizz'
+  elsif number % 5 == 0
+    return 'buzz'
+  else
+    return number
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,33 @@
 require 'fizzbuzz'
 
 describe 'FizzBuzz' do
-  it 'Outputs FizzBuzz sequence' do
-    # Test Cases
+  it 'Returns fizzbuzz sequence for range 1 - 20' do
+    (1..20).each{|n|
+      if n % 15 == 0
+        expect(fizzbuzz(n)).to eq 'fizzbuzz'
+      elsif n % 5 == 0
+        expect(fizzbuzz(n)).to eq 'buzz'
+      elsif n % 3 == 0
+        expect(fizzbuzz(n)).to eq 'fizz'
+      else
+        expect(fizzbuzz(n)).to eq n
+      end
+    }
   end
 end
+
+=begin
+expect(fizzbuzz(1)).to eq 1
+end
+it 'Returns 2 when the inputs is 2' do
+expect(fizzbuzz(2)).to eq 2
+end
+it 'Return fizz when the input is 3' do
+expect(fizzbuzz(3)).to eq 'fizz'
+end
+it 'Returns buzz when the inputs is 5' do
+expect(fizzbuzz(5)).to eq 'buzz'
+end
+it 'Returns fizzbuzz when the inputs divisible by three and 5' do
+expect(fizzbuzz(15)).to eq 'fizzbuzz'
+=end
